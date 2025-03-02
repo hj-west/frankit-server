c5faf02c4aa62bc88eb513a8954e6301216c8a2f
h.j_west <guswl9814@gmail.com>
2025-03-02 19:43:32 +0900

    init commit

---
+++ src/main/java/com/frankit/FrankitServerApplication.java
@@ -0,0 +1,13 @@
+package com.frankit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrankitServerApplication.class, args);
+    }
+
+}
