bdf4d2cf979a8d8eb71674b1be75b34a303cacbf
h.j_west <guswl9814@gmail.com>
2025-03-02 23:23:55 +0900

    BaseResponseStatus, BaseResponse 추가

---
+++ src/main/java/com/frankit/dto/BaseResponse.java
@@ -0,0 +1,33 @@
+package com.frankit.dto;
+
+import lombok.Setter;
+
+public class BaseResponse<T> {
+
+    protected BaseResponseStatus status;
+    protected T data;
+    @Setter
+    protected String message;
+
+    public BaseResponse() {
+        this.status = BaseResponseStatus.FRAN200000;
+        this.message = this.status.getMessage();
+    }
+
+    public BaseResponse(T data) {
+        this.status = BaseResponseStatus.FRAN200000;
+        this.data = data;
+        this.message = this.status.getMessage();
+    }
+
+    public BaseResponse(BaseResponseStatus status) {
+        this.status = status;
+        this.data = null;
+        this.message = this.status.getMessage();
+    }
+
+    public BaseResponse(BaseResponseStatus status, T data) {
+        this.status = status;
+        this.data = data;
+        this.message = this.status.getMessage();
+    }}
---
+++ src/main/java/com/frankit/dto/BaseResponseStatus.java
@@ -0,0 +1,33 @@
+package com.frankit.dto;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum BaseResponseStatus {
+    FRAN200000(HttpStatus.OK),
+    FRAN400000(HttpStatus.BAD_REQUEST),
+    FRAN401000(HttpStatus.UNAUTHORIZED),
+    FRAN402000(HttpStatus.PAYMENT_REQUIRED),
+    FRAN403000(HttpStatus.FORBIDDEN),
+    FRAN404000(HttpStatus.NOT_FOUND),
+    FRAN405000(HttpStatus.METHOD_NOT_ALLOWED),
+    FRAN500000(HttpStatus.INTERNAL_SERVER_ERROR),
+    FRAN501000(HttpStatus.NOT_IMPLEMENTED),
+    FRAN502000(HttpStatus.BAD_GATEWAY),
+    FRAN503000(HttpStatus.SERVICE_UNAVAILABLE),
+    FRAN504000(HttpStatus.GATEWAY_TIMEOUT),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    BaseResponseStatus(HttpStatus httpStatus) {
+        this(httpStatus, httpStatus.getReasonPhrase());
+    }
+
+    BaseResponseStatus(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
