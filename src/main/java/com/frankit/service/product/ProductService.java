65922b3567533761ebbfd13bbfef5e8fe6869f98
h.j_west <guswl9814@gmail.com>
2025-03-03 21:05:37 +0900

    상품 조회, 상품 삭제 메서드 생성 및 구현

--- src/main/java/com/frankit/service/product/ProductService.java
+++ src/main/java/com/frankit/service/product/ProductService.java
@@ -1,5 +1,10 @@
 package com.frankit.service.product;
 
+import com.frankit.entity.Product;
+import org.springframework.data.domain.Page;
+
 public interface ProductService {
+    Page<Product> findAllProducts(int page, int size);
     void saveProduct(String name, String description, Long price, Long shippingCost);
+    void deleteProduct(Long productId);
 }
--- src/main/java/com/frankit/service/product/ProductServiceImpl.java
+++ src/main/java/com/frankit/service/product/ProductServiceImpl.java
@@ -4,6 +4,8 @@ import com.frankit.entity.Product;
 import com.frankit.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,11 @@ public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
 
     @Override
+    public Page<Product> findAllProducts(int page, int size) {
+        return productRepository.findAll(PageRequest.of(page, size));
+    }
+
+    @Override
     @Transactional
     public void saveProduct(String name, String description, Long price, Long shippingCost) {
         productRepository.save(Product.builder()
@@ -23,5 +30,11 @@ public class ProductServiceImpl implements ProductService {
                 .price(price)
                 .shippingCost(shippingCost)
                 .build());
+    }
+
+    @Override
+    @Transactional
+    public void deleteProduct(Long productId) {
+        productRepository.deleteById(productId);
     }
 }
