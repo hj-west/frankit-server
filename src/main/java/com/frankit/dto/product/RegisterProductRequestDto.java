5411f1209d621f19e097d352bd8e956b27229ec3
h.j_west <guswl9814@gmail.com>
2025-03-03 20:46:16 +0900

    상품 등록용 request dto 생성

---
+++ src/main/java/com/frankit/dto/product/RegisterProductRequestDto.java
@@ -0,0 +1,29 @@
+package com.frankit.dto.product;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class RegisterProductRequestDto {
+    @Schema(description = "상품명", example = "모나미 볼펜")
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @Schema(description = "상품 설명", example = "기본에 춤실한 볼펜")
+    private String description;
+
+    @Schema(description = "상품 가격", example = "1000")
+    @NotNull
+    @NotBlank
+    private Long price;
+
+    @Schema(description = "배송비", example = "500")
+    private Long shippingCost;
+}
