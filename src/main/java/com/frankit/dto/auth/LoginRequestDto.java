fd0d029c5125fa083464ba2b6ea36a6468bcf927
h.j_west <guswl9814@gmail.com>
2025-03-03 18:38:09 +0900

    로그인 request dto에 스웨거 설정 추가

--- src/main/java/com/frankit/dto/auth/LoginRequestDto.java
+++ src/main/java/com/frankit/dto/auth/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.frankit.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,11 +12,13 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class LoginRequestDto {
+    @Schema(description = "사용자의 이메일", example = "user1@mail.com")
     @NotNull
     @NotBlank
     @Email
     private String email;
 
+    @Schema(description = "사용자의 패스워드", example = "user1")
     @NotNull
     @NotBlank
     private String password;
