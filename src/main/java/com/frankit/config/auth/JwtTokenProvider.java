6a59393edde33764a6b2fb55e79e21cdbef5747d
h.j_west <guswl9814@gmail.com>
2025-03-03 20:45:41 +0900

    토큰 유효성 검사 및 인증정보 확인용 메서드 추가

--- src/main/java/com/frankit/config/auth/JwtTokenProvider.java
+++ src/main/java/com/frankit/config/auth/JwtTokenProvider.java
@@ -1,7 +1,9 @@
 package com.frankit.config.auth;
 
 import com.frankit.config.properties.JwtTokenProperties;
+import com.frankit.service.auth.UserDetailsServiceImpl;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -9,6 +11,9 @@ import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -20,6 +25,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class JwtTokenProvider {
     private final JwtTokenProperties jwtTokenProperties;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
     private Key secretKey;
 
     /**
@@ -52,5 +58,50 @@ public class JwtTokenProvider {
                 .setExpiration(new Date(now.getTime() + jwtTokenProperties.getTokenValidTime()))
                 .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    /**
+     * 토큰에서 인증 정보 조회
+     *
+     * @param token JWT 토큰
+     * @return Authentication 객체
+     */
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(this.getUserEmail(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    /**
+     * 토큰에서 이메일 추출
+     *
+     * @param token JWT 토큰
+     * @return 사용자 이메일
+     */
+    public String getUserEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("email", String.class);
+    }
+
+    /**
+     * JWT 토큰의 유효성 및 만료 확인
+     *
+     * @param token JWT 토큰
+     * @return 유효 여부
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            log.error("Invalid JWT Token: {}", e.getMessage());
+            return false;
+        }
     }
 }
